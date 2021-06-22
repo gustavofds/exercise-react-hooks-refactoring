bd08bedd294569eb930e7c48d7c9924550602bc2
Gustavo Sousa <gustavohl98@gmail.com>
2021-06-22 16:09:42 -0300

    Refactored to use React Hooks instead of classes

--- src/TrafficSignal.jsx
+++ src/TrafficSignal.jsx
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import CarsContext from './context/CarsContext';
 import redSignal from './images/redSignal.jpeg';
 import yellowSignal from './images/yellowSignal.jpeg';
 import greenSignal from './images/greenSignal.jpeg';
+// import Cars from './Cars';
 
 const renderSignal = (signalColor) => {
   if (signalColor === 'red') return redSignal;
@@ -11,29 +12,27 @@ const renderSignal = (signalColor) => {
   return null;
 };
 
-class TrafficSignal extends Component {
-  render() {
-    const { color } = this.context.signal;
-    const { changeSignal } = this.context;
-    return (
-      <div>
-        <div className="button-container">
-          <button onClick={() => changeSignal('red')} type="button">
-            Red
-          </button>
-          <button onClick={() => changeSignal('yellow')} type="button">
-            Yellow
-          </button>
-          <button onClick={() => changeSignal('green')} type="button">
-            Green
-          </button>
-        </div>
-        <img className="signal" src={renderSignal(color)} alt="" />
+function TrafficSignal() {
+  const { signal: { color }, changeSignal } = useContext(CarsContext);
+
+  return (
+    <div>
+      <div className="button-container">
+        <button onClick={() => changeSignal('red')} type="button">
+          Red
+        </button>
+        <button onClick={() => changeSignal('yellow')} type="button">
+          Yellow
+        </button>
+        <button onClick={() => changeSignal('green')} type="button">
+          Green
+        </button>
       </div>
-    );
-  }
+      <img className="signal" src={renderSignal(color)} alt="" />
+    </div>
+  );
 };
 
-TrafficSignal.contextType = CarsContext;
+// TrafficSignal.contextType = CarsContext;
 
 export default TrafficSignal;
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,63 +1,50 @@
 // src/context/Provider.js
 
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import CarsContext from './CarsContext';
 
-class Provider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cars: {
-        red: false,
-        blue: false,
-        yellow: false,
-      },
-      signal: {
-        color: 'red',
-      },
-    }
-    this.moveCar = this.moveCar.bind(this);
-    this.changeSignal = this.changeSignal.bind(this);
-  }
+function Provider(props) {
+  const [cars, setCarPosition] = useState({
+    red: false,
+    blue: false,
+    yellow: false,
+  });
 
-  moveCar(car, side) {
-    this.setState({
-      cars: {
-        ...this.state.cars,
-        [car]: side,
-      },
+  const [signal, setSignalColor] = useState({
+    color: 'red',
+  })
+
+  const moveCar = function(car, side) {
+    setCarPosition({
+      ...cars,
+      [car]: side,    
     });
   };
 
-  changeSignal(signalColor) {
-    this.setState({
-      signal: {
-        ...this.state.signal,
-        color: signalColor,
-      },
-    });
+  const changeSignal = function (color) {
+    setSignalColor({
+      color,
+    })
+  }
+
+  
+  const context = {
+    cars,
+    signal,
+    moveCar,
+    changeSignal,
   };
 
-  render() {
-    const context = {
-      ...this.state,
-      moveCar: this.moveCar,
-      changeSignal: this.changeSignal,
-    };
-
-    const { children } = this.props;
-
-    return (
-      <CarsContext.Provider value={context}>
-        {children}
-      </CarsContext.Provider>
-    );
-  }
+  return (
+    <CarsContext.Provider value={context}>
+      {props.children}
+    </CarsContext.Provider>
+  );
 };
 
-Provider.propTypes = {
-  children: PropTypes.node.isRequired,
-};
+// Provider.propTypes = {
+//   children: PropTypes.node.isRequired,
+// };
 
 export default Provider;
