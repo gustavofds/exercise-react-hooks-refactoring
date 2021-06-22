c4701fe2015d43bd4659b61991118e5e917d24b4
Gustavo Sousa <gustavohl98@gmail.com>
2021-06-22 15:29:34 -0300

    Refactored to use React Hooks instead of classes

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,49 +1,38 @@
 // src/context/Provider.js
 
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import CarsContext from './CarsContext';
 
-class Provider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cars: {
-        red: false,
-        blue: false,
-        yellow: false,
-      },
-    }
-    this.moveCar = this.moveCar.bind(this);
-  }
+function Provider(props) {
+  const [cars, setCarPosition] = useState({
+    red: false,
+    blue: false,
+    yellow: false,
+  });
 
-  moveCar(car, side) {
-    this.setState({
-      cars: {
-        ...this.state.cars,
-        [car]: side,
-      },
+  const moveCar = function(car, side) {
+    setCarPosition({
+      ...cars,
+      [car]: side,    
     });
   };
 
-  render() {
-    const context = {
-      ...this.state,
-      moveCar: this.moveCar,
-    };
+  
+  const context = {
+    cars,
+    moveCar,
+  };
 
-    const { children } = this.props;
-
-    return (
-      <CarsContext.Provider value={context}>
-        {children}
-      </CarsContext.Provider>
-    );
-  }
+  return (
+    <CarsContext.Provider value={context}>
+      {props.children}
+    </CarsContext.Provider>
+  );
 };
 
-Provider.propTypes = {
-  children: PropTypes.node.isRequired,
-};
+// Provider.propTypes = {
+//   children: PropTypes.node.isRequired,
+// };
 
 export default Provider;
